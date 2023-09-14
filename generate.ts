b35fda00a1f56912ecab9f546626def6e504c3df
Matt Straathof <11823378+bruuuuuuuce@users.noreply.github.com>
2023-09-14 12:26:20 -0700

    fix: generate.ts script to generate to correct dir

--- generate.ts
+++ generate.ts
@@ -2,13 +2,15 @@ import * as path from "path";
 import * as glob from 'glob';
 import {execSync} from "child_process";
 
-const examplesGeneratedPath = path.resolve(__dirname, "src");
+// path where the generated code goes
+const generatedCodePath = path.resolve(__dirname, "src");
 
+// path to protos files
 const goodmetricsProtos = glob.sync(
     path.resolve(__dirname, 'proto', '**/*.proto')
 );
 
 // generating code from goodmetrics protos
-execSync(`PATH=node_modules/protoc-gen-ts/bin/:$PATH protoc --js_out=import_style=commonjs,binary:${examplesGeneratedPath} --proto_path=${__dirname} -I=/usr/local/include --ts_out=${examplesGeneratedPath} ${goodmetricsProtos.join(" ")}`, {
+execSync(`PATH=node_modules/protoc-gen-ts/bin/:$PATH protoc --js_out=import_style=commonjs,binary:${generatedCodePath} --proto_path=${__dirname} -I=/usr/local/include --ts_out=${generatedCodePath} ${goodmetricsProtos.join(" ")}`, {
     stdio: 'inherit',
 })
